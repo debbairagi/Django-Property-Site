a6da8fcde03ddfb3c7683ff746221771b63726f3
debbindubd <145745204+debbindubd@users.noreply.github.com>
2023-09-26 13:19:11 +0600

    Solve Static Problem (#2)

--- propertysite/propertysite/settings.py
+++ propertysite/propertysite/settings.py
@@ -125,12 +125,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATIC_URL = '/static/'
 
+# For Development:
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+
+
+# Optional, but required for production deployments
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
